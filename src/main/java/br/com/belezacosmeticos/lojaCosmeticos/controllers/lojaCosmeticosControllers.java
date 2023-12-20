c330df620138529b290fa63bc19aaa0e0e14d391
20201114010030 <20201114010030@SC391974.ifrn.local>
2023-12-20 13:42:49 -0300

    Eu criei o controller

---
+++ src/main/java/br/com/belezacosmeticos/lojaCosmeticos/controllers/lojaCosmeticosControllers.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.belezacosmeticos.lojaCosmeticos.controllers;
+
+/**
+ *
+ * @author 20201114010030
+ */
+public class lojaCosmeticosControllers {
+    
+}
